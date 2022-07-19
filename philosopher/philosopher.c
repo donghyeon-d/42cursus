43806802ad9e8bd0578be66014dfb191b58897c3
Donghyeon Choi <dongchoi@c5r4s6.42seoul.kr>
2022-07-19 12:15:27 +0900

    philosopher mutex function

--- philosopher/philosopher.c
+++ philosopher/philosopher.c
@@ -1,6 +1,7 @@
-#include <pthread.h>
 #include <stdio.h>
+#include <pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "philosopher.h"
 // #include "../ft_libft/libft.h"
 
@@ -59,13 +60,41 @@ void    philo_setting(t_data *data)
         return ;
     }
     i = -1;
-    while (++i < data->philo_cnt)
-        data->forks[i] = 1;
 }
 
 void *make_thread_handler(void *i)
 {
-    printf("create [%d]philo\n", *((int *)i));
+    t_data *data;
+    int a = -1;
+
+    data = (t_data *)i;
+    int id = data->philo->philo_id;
+    printf("I'm %d philo\n", id);
+    gettimeofday(&data->philo[id].t, NULL);
+    printf ("%d\n", data->philo[id].t.tv_usec - data->time.tv_usec);
+    (data->forks)[id].forks = malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init((data->forks)[id].forks, NULL);
+    if (id != 1)
+        a = pthread_mutex_lock((data->forks)[id].forks);
+    else if (id == 1)
+    {
+        while (a != 0)
+            a = pthread_mutex_lock((data->forks)[0].forks);
+    }
+        // a = pthread_mutex_lock((data->forks)[0].forks);
+    if (a == 0)
+        printf("%d locked\n", id);
+    sleep(3);
+    a = pthread_mutex_unlock((data->forks)[id].forks);
+    if (a == 0)
+        printf("%d unlocked %d\n", id, a);
+    a = pthread_mutex_unlock((data->forks)[id].forks);
+    if (a == 0)
+        printf("%d unlocked %d\n", id, a);
+    // if (id > 0)
+        // pthread_mutex_init((data->forks)[id - 1].forks, NULL);
+    gettimeofday(&data->philo[id].t, NULL);
+    printf ("%d\n", data->philo[id].t.tv_usec - data->time.tv_usec);
     return (NULL);
 }
 
@@ -83,8 +112,11 @@ t_data  *init_data(int argc, char *argv[])
     data->time_to_die = ft_atoi_positive(argv[2]);
     data->time_to_eat = ft_atoi_positive(argv[3]);
     data->time_to_sleep = ft_atoi_positive(argv[4]);
-    data->must_eat = 2147483647;
     data->error = 0;
+    data->forks = malloc(sizeof(t_forks));
+    if (data->forks == NULL)
+        data->error = 1;
+    data->must_eat = 2147483647;
     if (argc == 6)
         data->must_eat = ft_atoi_positive(argv[5]);
     if (data->philo_cnt == -1 || data->time_to_die == -1 || \
@@ -100,19 +132,32 @@ t_data  *init_data(int argc, char *argv[])
 int main(int argc, char *argv[])
 {
     t_data *data;
+    int a;
 
     data = init_data(argc, argv);
     if (data == NULL || data->error == 1)
+    {
+        printf("argv!\n");
         return (0);
+    }
+    gettimeofday(&data->time, NULL);
     for (int i = 0; i < data->philo_cnt; i++)
     {
-        int a = pthread_create(data->philo->philo[i], NULL, make_thread_handler, (void *)&i);
-        printf("%d %d\n", a, i);
-        if (a != 0)
-        {
-            printf("wrong\n");
-            exit(0);
-        }
+        data->philo->philo_id = i;
+        a = pthread_create(&data->philo->philo[i], NULL, make_thread_handler, (void *)data);
+        sleep(1);
     }
+    // a = pthread_join((data->philo->philo)[0], NULL);
+    // pthread_mutex_t *f = malloc(sizeof(pthread_mutex_t));
+    // a = pthread_mutex_init(f, NULL);
+    // printf
+    for (int i = 0; i < data->philo_cnt; i++)
+    {
+            (data->forks)[i].forks = malloc(sizeof(pthread_mutex_t));
+        a = pthread_mutex_init((data->forks)[i].forks, NULL);
+    }
+    sleep(1);
+    for (int i = 0; i < data->philo_cnt; i++)
+        pthread_join(data->philo->philo[i], NULL);
     return (0);
 }
--- philosopher/philosopher.h
+++ philosopher/philosopher.h
@@ -1,23 +1,44 @@
 #include <pthread.h>
+#include <sys/time.h>
 
 #ifndef PHILOSOPHER_H
 #define PHILOSOPHER_H
 
+typedef enum e_status {
+    sleep = 0,
+    thinking = 1,
+    fork = 2,
+    eating = 3,
+    die = -1
+} t_status;
+
 typedef struct  s_philo
 {
-    pthread_t **philo;
+    pthread_t       *philo;
+    int             id;
+    struct timeval  t;
+    t_status        status;
+    int             last_eat;
 }   t_philo;
+
+typedef struct  s_forks
+{
+    int             idx;
+    int             status;
+    pthread_mutex_t *forks;
+} t_forks;
 
 typedef struct  s_data
 {
-    int philo_cnt;
-    int time_to_die;
-    int time_to_eat;
-    int time_to_sleep;
-    int must_eat;
-    int error;
-    int *forks;// 1 사용가능  0 사용 중 
+    int     philo_cnt;
+    int     time_to_die;
+    int     time_to_eat;
+    int     time_to_sleep;
+    int     must_eat;
+    int     error;
+    struct  timeval time;
     t_philo *philo;
+    t_forks *forks;
 }   t_data;
 
 
