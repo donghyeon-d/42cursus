9d2e8e5315e14f40a282c940a9894d877957bf78
dong_d <ehdgud6634@gmail.com>
2022-11-08 21:42:46 +0900

    cpp02 init

---
+++ cpp_module/cpp_module_02/ex00/Fixed.hpp
@@ -0,0 +1,17 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+	private:
+		int				 fixedPointNum;
+		static const int	fractionalBit;
+
+	public:
+        Fixed();
+        ~Fixed();
+        Fixed
+
+};
+
+#endif
