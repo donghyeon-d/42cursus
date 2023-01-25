b83ae3b8e4678815c614f892db73690c8c4170c7
donghyeon <ehdgud6634@gmail.com>
2023-01-26 01:08:26 +0900

    cpp08 ex02 use stack's protected member

--- cpp_module/cpp_module_08/ex02/MutantStack.hpp
+++ cpp_module/cpp_module_08/ex02/MutantStack.hpp
@@ -3,11 +3,44 @@
 #include <stack>
 #include <deque>
 
-template <typename T>
-class MutantStack : public std::stack<T>
+// template <typename T>
+// class MutantStack : public std::stack<T>
+// {	
+// 	private:
+// 		std::deque<T> data_;
+// 	public:
+// 		MutantStack() {}
+// 		MutantStack(const MutantStack &mutantStack) {
+// 			*this = mutantStack; }
+// 		~MutantStack() {}
+// 		MutantStack &operator=(const MutantStack &mutantStack) {
+// 			if (this != &mutantStack) {
+// 				data_ = mutantStack.data_; }
+// 			return (*this);
+// 		}
+// 		T &top() { return (data_.front()); }
+// 		const T &top() const { return (data_.front()); }
+// 		bool empty() const { return (data_.empty()); }
+// 		size_t size() const { return (data_.size()); }
+// 		void push(const T& value) { data_.push_front(value);	}
+// 		void pop() { data_.pop_front(); }
+// 		typedef typename std::deque<T>::iterator iterator;
+// 		iterator begin() { return (data_.begin()); }
+// 		iterator end() { return (data_.end()); }
+// 		typedef typename std::deque<T>::const_iterator const_iterator;
+// 		const_iterator begin() const { return (data_.begin()); }
+// 		const_iterator end() const { return (data_.end()); }
+// 		void printAllData() const
+// 		{
+// 			for (const_iterator itr = begin(); itr != end(); itr++)
+// 				std::cout << *itr << std::endl;
+// 		}
+// };
+
+
+template <typename T, typename C = std::deque<T> >
+class MutantStack : public std::stack<T, C>
 {	
-	private:
-		std::deque<T> data_;
 	public:
 		MutantStack() {}
 		MutantStack(const MutantStack &mutantStack) {
@@ -15,26 +48,29 @@ class MutantStack : public std::stack<T>
 		~MutantStack() {}
 		MutantStack &operator=(const MutantStack &mutantStack) {
 			if (this != &mutantStack) {
-				data_ = mutantStack.data_; }
+				this->c = mutantStack.c;
+			 }
 			return (*this);
 		}
-		T &top() { return (data_.front()); }
-		const T &top() const { return (data_.front()); }
-		bool empty() const { return (data_.empty()); }
-		size_t size() const { return (data_.size()); }
-		void push(const T& value) { data_.push_front(value);	}
-		void pop() { data_.pop_front(); }
-		typedef typename std::deque<T>::iterator iterator;
-		iterator begin() { return (data_.begin()); }
-		iterator end() { return (data_.end()); }
-		typedef typename std::deque<T>::const_iterator const_iterator;
-		const_iterator begin() const { return (data_.begin()); }
-		const_iterator end() const { return (data_.end()); }
+		typedef typename C::iterator iterator;
+		iterator begin()	{	return (this->c.begin());	}
+		iterator end()	{	return (this->c.end());	}
+		typedef typename C::const_iterator const_iterator;
+		const_iterator begin() const	{	return (this->c.begin());	}
+		const_iterator end() const	{	return (this->c.end());	}
+		typedef typename C::reverse_iterator reverse_iterator;
+		reverse_iterator rbegin()	{	return (this->c.rbegin());	}
+		reverse_iterator rend()	{	return (this->c.rend());	}
+		typedef typename C::const_reverse_iterator const_reverse_iterator;
+		const_reverse_iterator rbegin() const	{	return (this->c.rbegin());	}
+		const_reverse_iterator rend() const	{	return (this->c.rend());	}
+
 		void printAllData() const
 		{
 			for (const_iterator itr = begin(); itr != end(); itr++)
 				std::cout << *itr << std::endl;
 		}
 };
+
 
 #endif
--- cpp_module/cpp_module_08/ex02/main.cpp
+++ cpp_module/cpp_module_08/ex02/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stack>
 #include <deque>
+#include <list>
 #include "MutantStack.hpp"
 
 
@@ -36,7 +37,9 @@ int main()
 			std::cout << *it << std::endl;
 			++it;
 		}
-		std::stack<int> s(mstack);
+		std::stack<int> s(mstack); // std::stack 상속 제대로 받아서 처리해주는 방식 아니면 이게 안됨.
+		std::cout << s.size() << std::endl;
+		std::cout << s.top() << std::endl;
 	}
 	{
 		std::cout << "\n< --- My test --- >" << std::endl;
@@ -47,12 +50,31 @@ int main()
 		mstack.push(4);
 		mstack.push(5);
 		std::cout << "top() : " << mstack.top() << std::endl;
-		std::cout << "< --- print all [begin == top] --- >" << std::endl;
+		std::cout << "< --- print all --- >" << std::endl;
 		mstack.printAllData();
 
 		std::cout << "< --- copy test --- >" << std::endl;
 		MutantStack<int> mstack_copy(mstack);
 		mstack_copy.printAllData();
+	}
+	{
+		std::cout << "\n< --- My test --- >" << std::endl;
+		std::list<int> mstack;
+		mstack.push_front(1);
+		mstack.push_front(2);
+		mstack.push_front(3);
+		mstack.push_front(4);
+		mstack.push_front(5);
+		std::cout << "top() : " << mstack.front() << std::endl;
+		std::cout << "< --- print all --- >" << std::endl;
+		for (std::list<int>::iterator itr = mstack.begin(); itr != mstack.end(); itr++)
+			std::cout << *itr << std::endl;
+		// mstack.printAllData();
+
+		std::cout << "< --- copy test --- >" << std::endl;
+		std::list<int> mstack_copy(mstack);
+		for (std::list<int>::iterator itr = mstack.begin(); itr != mstack.end(); itr++)
+			std::cout << *itr << std::endl;
 	}
 	return 0;
 }
