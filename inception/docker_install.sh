de7401bc8fee226c5a1c8fd85879c0e3f8fccf06
Dong_D <ehdgud6634@gmail.com>
2023-03-07 20:40:21 +0900

    Inception add docker-compose installation in shell script

--- inception/docker_install.sh
+++ inception/docker_install.sh
@@ -19,5 +19,10 @@ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev
 # update apt package
 sudo apt-get update
 
-# install Docker engine, Container, Docker Compose
+# install Docker engine, Container
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# install Docker Compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
