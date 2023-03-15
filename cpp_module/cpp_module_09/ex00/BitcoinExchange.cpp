810d97c9c8424560a3bee7a22fe50fea64b349c7
Dong_D <ehdgud6634@gmail.com>
2023-03-15 23:29:17 +0900

    cpp09 WIP

--- cpp_module/cpp_module_09/ex00/BitcoinExchange.cpp
+++ cpp_module/cpp_module_09/ex00/BitcoinExchange.cpp
@@ -7,16 +7,25 @@ BitcoinExchange::BitcoinExchange() {
 BitcoinExchange::BitcoinExchange(char *file) {
 	std::fstream readfile(file);
 	t_data data;
+
 	if (readfile.is_open() == false) {
-		return ;
+		throw InvalidFileException();
 	}
 	else {
-		string line;
+		std::string line;
+		int line_num(1);
 		while (!readfile.eof())
 		{
 			std::getline(readfile, line);
-			data = parse_line(line);
+			if (line_num == 1) {
+				if (line != "date | value") {
+					data.error = BADINPUT;
+				}
+			}
+			else
+				data = parse_input_line(line);
 			_data.push_back(data);
+			line_num++;
 		}
 	}
 }
@@ -34,44 +43,115 @@ BitcoinExchange::~BitcoinExchange() {
 
 }
 
-t_data BitcoinExchange::parse_line(std::string &line) {
-	t_date data;
+void BitcoinExchange::getCSV() {
+	std::fstream readfile(CSVFILE);
+
+	if (readfile.is_open() == false) {
+		throw InvalidFileException();
+	}
+	else {
+		std::string line;
+		int line_num(1);
+		while (!readfile.eof())
+		{
+			std::getline(readfile, line);
+			if (line_num == 1) {
+				if (line != "date,exchange_rate") {
+					throw InvalidFileException();
+				}
+			}
+			else {
+				if (!valid_check_data_line(line)) {
+					throw InvalidFileException();
+				}
+				else {
+					_rate[line.substr(0, 10)] = ft_stof(line.substr(11));
+				}
+			}
+			line_num++;
+		}
+	}
+}
+
+const char *BitcoinExchange::InvalidFileException::what() const throw() {
+	return ("Error: Invalid File");
+}
+
+bool BitcoinExchange::valid_check_data_line(std::string &line) {
+	if (line.length() < 12) {
+		return false;
+	}
+
+	if (line[4] != '-' || line[7] != '-' || line[10] != ',') {
+		return false;
+	}
+
+	bool used_dot(false);
+	for (unsigned int i = 11; i < line.length(); i++) {
+		if (used_dot == true && line[i] == '.')
+			return false;
+		if (!std::isdigit(line[i]) && line[i] != '.')
+			return false;
+		if (line[i] != '.')
+			used_dot = true;
+	}
+
+	int year(std::atoi(line.substr(0, 4).c_str()));
+	int month(std::atoi(line.substr(5, 2).c_str()));
+	int day(std::atoi(line.substr(8, 2).c_str()));
+	if ((year < 2009 || 2023 < year) || \
+		(month < 1 || month > 12) || \
+		(day < 1 || day > 31) || \
+		(month == 2 && day > 28) || \
+		((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)) {
+		return false;
+	}
+	return true;
+}
+
+float ft_stof(std::string str) {
+	std::stringstream ssFloat(str);
+	float value(0);
+	ssFloat >> value;
+	return value;
+}
+
+t_data BitcoinExchange::parse_input_line(std::string &line) {
+	t_data data;
 	data.error = TRUE;
 
 	if (line.length() < 14) {
 		data.error = BADINPUT;
 		return data;
 	}
-	int year(std::atoi(line.substr(0, 4)));
-	int month(std::atoi(line.substr(5, 2)));
-	int day(std::atoi(line.substr(8, 2)));
+	int year(std::atoi(line.substr(0, 4).c_str()));
+	int month(std::atoi(line.substr(5, 2).c_str()));
+	int day(std::atoi(line.substr(8, 2).c_str()));
 
 	data.date = line.substr(0, 10);
 	data.value = line.substr(13);
-	std::stringstream ssFloat(data.value);
-	float value(0);
-	ssFloat >> value;
 
 	if ((line[4] != '-' || line[7] != '-' || line.substr(10, 3) != " | ")) {
 		data.date = "Wrong Format";
 		data.error = BADINPUT;
 		return data;
 	}
-	for (int i = 13; i < line.length(); i++) {
-		if (std::isdigit(line[i]) == flase){
+	for (unsigned int i = 13; i < line.length(); i++) {
+		if (std::isdigit(line[i]) == false){
 			data.date = "Wrong Format";
 			data.error = BADINPUT;
 			return data;
 		}
 	}
-	if (year < 2009 || 2023 < year) || \
+	if ((year < 2009 || 2023 < year) || \
 		(month < 1 || month > 12) || \
 		(day < 1 || day > 31) || \
 		(month == 2 && day > 28) || \
-		((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+		((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)) {
 		data.error = BADINPUT;
 		return data;
 	}
+	float value(ft_stof(data.value));
 	if (value > 999) {
 		data.error = TOOLARGE;
 		return data;
@@ -80,6 +160,7 @@ t_data BitcoinExchange::parse_line(std::string &line) {
 		data.error = NOTPOSITIVE;
 		return data;
 	}
+	return data;
 }
 
 void	BitcoinExchange::print_one_data(t_data data) {
@@ -93,7 +174,7 @@ void	BitcoinExchange::print_one_data(t_data data) {
 			std::cout << "Error: not a positive number." << std::endl;
 			break ;
 		case BADINPUT:
-			std::cout << Error: bad input << " => " << data.date << std::endl;
+			std::cout << "Error: bad input" << " => " << data.date << std::endl;
 			break ;
 		case TOOLARGE:
 			std::cout << "Error: too large a number." << std::endl;
@@ -105,10 +186,10 @@ bool	BitcoinExchange::exchange() {
 	if (_data.empty() == true)
 		return false;
 
-	float value(0);
 	for (std::deque<t_data>::iterator it = _data.begin(); it != _data.end(); it++) {
-		std::stringstream ssFloat(data.value);
-		ssFloat >> exchanged_value;
+		std::stringstream ssFloat(it->value);
+		ssFloat >> it->exchanged_value;
+		it->exchanged_value *= 
 		// TODO : exchange 어떻게?
 	}
 	return true;
--- cpp_module/cpp_module_09/ex00/BitcoinExchange.hpp
+++ cpp_module/cpp_module_09/ex00/BitcoinExchange.hpp
@@ -5,25 +5,36 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <cctype>
+#include <except>
 
 #define TRUE 1
 #define NOTPOSITIVE -1
 #define BADINPUT -2
 #define TOOLARGE -3
+#define CSVFILE "./data.csv"
 
 typedef struct s_data {
 	std::string date;
 	std::string value;
 	float exchanged_value;
-	int err;
+	int error;
 } t_data;
 
 class BitcoinExchange {
 	private:
 		std::deque<t_data> _data;
-		t_data parse_line(std::string &line);
+		std::map<std::string, float> _rate;
+		bool valid_check_data_line(std::string &line);
+		t_data parse_input_line(std::string &line);
 		void	print_one_data(t_data data);
+		void getCSV();
+		class InvalidFileException : public std::exception
+        {
+            public :
+                const char *what() const throw();
+        };
 	public:
 		BitcoinExchange();
 		BitcoinExchange(char *file);
@@ -31,8 +42,10 @@ class BitcoinExchange {
 		BitcoinExchange &operator=(const BitcoinExchange &ref);
 		~BitcoinExchange();
 		bool exchange();
-		bool is
+		// bool is
 		void print_exchanging_result();
 };
+
+float ft_stof(std::string str);
 
 #endif
