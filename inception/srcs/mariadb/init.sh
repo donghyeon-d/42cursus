9635e13bb1f923a54bd68acd652a51464f78dec3
Dong_D <ehdgud6634@gmail.com>
2023-03-23 22:50:24 +0900

    Inception mandatory

--- inception/srcs/mariadb/init.sh
+++ inception/srcs/mariadb/init.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
 
-service mysql start
 
-mysql << end
-UPDATE mysql.user SET Password=PASSWORD('123123') WHERE User='root' AND Host='mariadb';
-CREATE DATABASE IF NOT EXISTS wordpress_db;
-CREATE USER 'dongchoi'@'%' IDENTIFIED BY '123123';
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'dongchoi'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-end
-
-touch /var/log/mariadb.error.log /var/log/mariadb.general.log
-# chown -R www-data:www-data /var/log/mariadb.error.log
-# chown -R www-data:www-data /var/log/mariadb.general.log
-chmod -R 775 /var/log/mariadb.error.log
-chmod -R 775 /var/log/mariadb.general.log
-echo "log-error = /var/log/mariadb.error.log" >> /etc/mysql/conf.d/mysql.cnf
-echo "general_log=on" >> /etc/mysql/conf.d/mysql.cnf
-echo "general_log_file=/var/log/mariadb.general.log" >> /etc/mysql/conf.d/mysql.cnf
-
-
-#service mysql stop
-
-# #UPDATE mysql.user SET Password=PASSWORD('123123') WHERE User='root' AND Host='127.0.0.1';
 # mysql << end
-# CREATE DATABASE wordpress_db;
-# CREATE USER 'dongchoi'@'%' IDENTIFIED BY '123123';
-# GRANT ALL ON wordpress_db.* TO 'dongchoi'@'%';
-# FLUSH PRIVILEGES;
+
+echo "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;
+CREATE USER '$MARIADB_USER_NAME'@'%' IDENTIFIED BY '$MARIADB_USER_PW';
+GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER_NAME'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;" > $MARIADB_DATABASE.sql;
+service mysql start && mysql < $MARIADB_DATABASE.sql;
+rm $MARIADB_DATABASE.sql;
+
 # end
 
-#mysql << end
-# UPDATE mysql.user SET Password=PASSWORD('$MARIADB_ADMIN_PASSWORD') WHERE User='$MARIADB_ADMIN_USER' AND Host='$MARIADB_DATABASE_HOST';
-# CREATE DATABASE $MARIADB_DATABASE;
-# CREATE USER '$MARIADB_ID'@'%' IDENTIFIED BY '$MARIADB_PW';
-# GRANT ALL ON $MARIADB_DATABASE.* TO '$MARIADB_ID'@'%';
-# FLUSH PRIVILEGES;
-#end;
+# if [ ! -f "/var/log/mariadb.error.log" ]; then
+# touch /var/log/mariadb.error.log /var/log/mariadb.general.log
+# chown -R www-data:www-data /var/log/mariadb.error.log
+# chown -R www-data:www-data /var/log/mariadb.general.log
+# chmod -R 775 /var/log/mariadb.error.log
+# chmod -R 775 /var/log/mariadb.general.log
+# echo "log-error = /var/log/mariadb.error.log" >> /etc/mysql/conf.d/mysql.cnf
+# echo "general_log=on" >> /etc/mysql/conf.d/mysql.cnf
+# echo "general_log_file=/var/log/mariadb.general.log" >> /etc/mysql/conf.d/mysql.cnf
+# # wp config create --dbname="wordpress_db" --dbuser="dongchoi" --dbpass="123123" --dbhost="mariadb" --path="/var/www/html" --dbcharset="utf8" --allow-root
+# fi
+
+service mysql stop
+
+# exec "$@"
+
+mysqld
+# --bind-address=0.0.0.0
--- inception/srcs/wordpress/init.sh
+++ inception/srcs/wordpress/init.sh
@@ -1,29 +1,41 @@
 #!/bin/bash 
 
-if [ ! -f "/var/www/html/wp-config.php" ]; then
 # wordpress 설치
-wget -O /tmp/wordpress.tar.gz "https://wordpress.org/latest.tar.gz"
-tar -xvzf /tmp/wordpress.tar.gz -C /tmp
-mkdir -p /var/www/html
-mv /tmp/wordpress/* /var/www/html/
-chown -R www-data:www-data /var/www/html 
-chmod -R 775 /var/www/html
-rm -r /tmp/wordpress /tmp/wordpress.tar.gz
+# wget -O /tmp/wordpress.tar.gz "https://wordpress.org/latest.tar.gz"
+# tar -xvzf /tmp/wordpress.tar.gz -C /tmp
+# mkdir -p /var/www/html
+# mv /tmp/wordpress/* /var/www/html/
+# chown -R www-data:www-data /var/www/html 
+# chmod -R 775 /var/www/html
+# rm -r /tmp/wordpress /tmp/wordpress.tar.gz
 
 # config 생성
-wp config create --dbname="wordpress_db" --dbuser="dongchoi" --dbpass="123123" --dbhost="mariadb" --path="/var/www/html" --dbcharset="utf8" --allow-root
-fi
 
-# log file 
-touch /var/log/fpm-php.www.log
-chown -R www-data:www-data /var/log/fpm-php.www.log
-chmod -R 775 /var/log/fpm-php.www.log
-touch /var/log/fpm-php.access.log
-chown -R www-data:www-data /var/log/fpm-php.access.log
+# if [ ! -f "/var/www/html/wp-config.php" ]; then
+wp --allow-root core download --locale=ko_KR --path=/var/www/html --force
+wp --allow-root config create --dbname="$MARIADB_DATABASE" --dbuser="$MARIADB_USER_NAME" --dbpass="$MARIADB_USER_PW" --dbhost="$MARIADB_HOST" --path="/var/www/html" --dbcharset="utf8"
+wp core install --path=/var/www/html --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root --skip-email
+wp --allow-root --path=/var/www/html user create "$WP_ADMIN_USER" "$WP_ADMIN_EMAIL" --user_pass="$WP_ADMIN_PASSWORD" --role=administrator || echo "User($WP_ADMIN_USER) already exists." 
+wp user create --path=/var/www/html "$WP_USER" "$WP_EMAIL" --user_pass="$WP_PASSWORD" --role=editor  --allow-root || echo "User($WP_USER) already exists."
+
+# echo "access.log = /var/log/php7.3-fpm.access.log;" >> /var/www/html/wp-config.php
+# echo "php_flag[display_errors] = on;" >> /var/www/html/wp-config.php
+# echo "php_admin_value[error_log] = /var/log/php7.3-fpm.error.log;" >> /var/www/html/wp-config.php
+# echo "php_admin_flag[log_errors] = on;" >> /var/www/html/wp-config.php
+# fi
+
+if [ ! -f "/var/log/php7.3-fpm.error.log" ]; then
+touch /var/log/php7.3-fpm.error.log
+chown -R www-data:www-data /var/log/php7.3-fpm.error.log
+chmod -R 775 /var/log/php7.3-fpm.error.log
+touch /var/log/php7.3-fpm.access.log
+chown -R www-data:www-data /var/log/php7.3-fpm.access.log
 chmod -R 775 /var/log/fpm-php.access.log
+fi
 
 service php7.3-fpm start
 service php7.3-fpm stop
+
 php-fpm7.3 -F -R
 
 #echo "env[MARIADB_HOST] = \$MARIADB_HOST" >> /etc/php/7.4/fpm/pool.d/www.conf
